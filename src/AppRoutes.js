155336d59512e869c9dd040b8e40cef30eae7c54
Christian <chrisplatzi@outlook.com>
2020-12-07 14:38:27 -0600

    optimizacion codigo v3

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -1,20 +1,25 @@
 import { BrowserRouter as Router,
          Switch,
-         Route
-        }  from 'react-router-dom';
+         Route,
+         Redirect
+        }  from 'react-router-dom'
 
 import  Users  from './components/Users';
 import EditUser  from './components/EditUser';
 import  NewUser  from './components/NewUser';
+
         const AppRoutes = () => {
             return (
+                    
                     <Router>
                         <Switch>
                         <Route path="/users" exact component={ Users } />
                         <Route path="/users/edit/:userId" component={ EditUser } />
-                        <Route path="/users/new" component={ NewUser } />
+                        <Route path="/users/new" component={ NewUser } />            
                         </Switch>
+                        <Redirect from='/' to='/users'/>
                     </Router>
+                    
             )
         }
 
